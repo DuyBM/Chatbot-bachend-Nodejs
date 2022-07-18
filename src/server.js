9b535259e553ee34ce4ac3bbb7c789099f013560
duy <bluemay51998@gmail.com>
2022-07-18 18:53:06 +0700

    deploy heroku

---
+++ src/server.js
@@ -0,0 +1,28 @@
+import express from "express";
+import viewEngine from "./config/viewEngine";
+import initWebRoute from "./routes/web";
+import bodyParser from "body-parser";
+require("dotenv").config();
+
+
+
+let app = express()
+
+//config viewengine
+
+viewEngine(app);
+
+//parser request to json
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended : true}));
+
+//init web routes 
+
+initWebRoute(app);
+
+let port = process.env.PORT || 8080;
+
+app.listen(port, ()=>{
+    console.log("chatbot is running at port: " +port);
+})
